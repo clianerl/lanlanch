c75ff34165c4f20c8b375109555a8e7ef62f840d
zhangmy <784704506@qq.com>
2018-01-27 18:08:34 +0800

    助理的wx
    Signed-off-by: zhangmy <784704506@qq.com>

--- server/connection.js
+++ server/connection.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
+  host     : '114.115.132.9',
+  user     : 'admin',
   password : 'chonglou159',
   database : 'lanlan'
 });
--- server/routepath.js
+++ server/routepath.js
@@ -1,10 +1,12 @@
 var index = require('./routes/index');
 var users = require('./routes/users');
 var getMessage = require('./routes/interfaces/getMessage');
+var pullwx = require('./routes/interfaces/pullwx');
 
 function setPath(app){
 	app.use('/', index);
 	app.use('/users', users);
 	app.use('/node/getMessage',getMessage);
+	app.use('/wx',pullwx);
 }
 module.exports = setPath
