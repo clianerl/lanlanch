38b08e7d77b224cc7a4be74172d24cf04cefdcb9
clianel <yanglan0_o@sina.com>
2018-01-28 23:25:18 +0800

    lan

--- server/config.js
+++ server/config.js
@@ -15,14 +15,21 @@ var config = {
 			return true;
 		}
 	},
-	getAccessToken:function(){
+	getAccessToken:function(doFun){
 		//记录请求次数，最多五次
 		this.regettime++;
 		if(this.regettime>this.max_regettime){
+			console.log("超出第"+this.max_regettime+"次请求微信服务器～不再请求～");
+			console.log("获取微信access_token失败！！！请检查网络连接～");
+			return false;
+		}
+		console.log("第"+this.regettime+"次请求微信服务器～");
 		console.log("==========getAccessToken start=======")
 		if(this.accessTokenIsRight()){
-			return this.access_token;
+			console.log("!!already had access_token!!"+this.access_token);
+			doFun(this.access_token);
 		}
+		console.log("!!get new access_token!!")
 		var https = require('https'); 
 		var qs = require('querystring'); 
    
@@ -38,19 +45,34 @@ var config = {
 		    hostname: 'api.weixin.qq.com', 
 		    // port: 10086, 
 		    path: '/cgi-bin/token?' + content, 
-		    method: 'GET' 
+		    method: 'GET'
 		}; 
+		var $this = this;
+		console.log('我要开始发送get请求了！'); 
 		var req = https.request(options, function (res) { 
-			console.log('OPTIONS: ' + JSON.stringify(options)); 
-		    console.log('STATUS: ' + res.statusCode); 
-		    console.log('HEADERS: ' + JSON.stringify(res.headers)); 
+			console.log('--OPTIONS: ' + JSON.stringify(options)); 
+		    console.log('--STATUS: ' + res.statusCode); 
+		    console.log('--HEADERS: ' + JSON.stringify(res.headers)); 
 		    res.setEncoding('utf8'); 
 		    var body = "";
 		    res.on('data', function (chunk) { 
-		    	body+=chunk;
+		    	body += chunk;
 		    }); 
 		    res.on('end',function(){
 		    	console.log('BODY: ' + body); 
+		    	var bodyData = eval('('+body+')');
+		    	if(bodyData.errcode!=undefined){
+		    		//如果errcode不为空，则请求失败，重新发送请求
+		    		setTimeout(function(){$this.getAccessToken(doFun)},"1000");
+		    	}else{
+		    		//请求成功，将token存入config
+		    		console.log('成功获取access_token！！！开心！！！'); 
+		    		$this.access_token = bodyData.access_token;
+		    		$this.expires_in = bodyData.expires_in;
+		    		$this.starttime = new Date().getTime();
+		    		$this.regettime = 0;
+		    		console.log("success!!config:"+JSON.stringify($this));
+		    		doFun($this.access_token);
 		    	}
 		    })
 		}); 
@@ -58,8 +80,9 @@ var config = {
 		req.on('error', function (e) { 
 		    console.log('problem with request: ' + e.message); 
 		}); 
-		   
+		//node 发送请求时，request也必须end，否则会 socket hang up
 		req.end();
+		
 	}
 }
 
