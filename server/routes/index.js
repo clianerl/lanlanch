dd90c3c1adc4199c0b7b6b04da516f8ab720dafc
clianel <yanglan0_o@sina.com>
2018-02-01 23:36:15 +0800

    lalala

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,10 @@ var conn = require('../connection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	conn.query("SELECT * FROM lanlan.message;",function(data){
-		res.render('index', { title: 'Express' });
-	})
+	// conn.query("SELECT * FROM lanlan.lan_message;",function(data){
+	// 	res.render('index', { title: 'Express' });
+	// })
+	res.send("Welcome!!!");
 });
 
 module.exports = router;
